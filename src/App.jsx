7b0706fcbec491c6c7bca581068683d999ac861c
Aarushi42 <aarushikoirala207@gmail.com>
2026-03-07 21:36:01 +0530

    auth, feed only when logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
